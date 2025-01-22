8ffd41c8d243eafb4b95631b4428c6fa3bc754de
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-22 17:54:06 +0300

    create first server example

--- 01-Node-Farm/index.js
+++ 01-Node-Farm/index.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Request has just hit');
+  console.lof(req);
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log(`Server is listening on http://127.0.0.1:8000`);
+});
 
 //Synchronous file reading
-
 /*
 const fileContent = fs.readFileSync('./read.txt', 'utf-8');
 
@@ -9,10 +18,10 @@ console.log(fileContent);
 
 fs.writeFileSync('./out-put.txt', fileContent);
 console.log('File has been written');
-*/
+
 
 //Asynchronous file reading
-
+/*
 console.log('Asynchronous reading started');
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
   if (err) return console.log(err);
@@ -24,3 +33,4 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
   });
 });
 console.log('Asynchronous reading finished');
+*/
