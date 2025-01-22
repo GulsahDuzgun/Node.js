a2c476cd6369e496241f315dbd5d1cbdd13c369e
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-22 16:42:35 +0300

    asynchronous file reading

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ console.log('File has been written');
 */
 
 //Asynchronous file reading
+
+console.log('Asynchronous reading started');
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  if (err) return console.log(err);
+  fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data) => {
+    console.log(data);
+    fs.writeFile('./txt/output.txt', data, 'utf-8', (err) => {
+      if (err) return console.log(err);
+    });
+  });
+});
+console.log('Asynchronous reading finished');
