eefb8bf7e28719c5781acd3a32e9da0043e9e85c
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-22 16:33:49 +0300

    added files

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 
+//Synchronous file reading
+
+/*
 const fileContent = fs.readFileSync('./read.txt', 'utf-8');
 
 console.log(fileContent);
 
 fs.writeFileSync('./out-put.txt', fileContent);
 console.log('File has been written');
+*/
+
+//Asynchronous file reading
