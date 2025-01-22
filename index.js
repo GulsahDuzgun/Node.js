1395153d2403d6cb58d71752cc2a7693c26d90ce
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-22 14:32:44 +0300

    output file is added

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const fs = require('fs');
 const fileContent = fs.readFileSync('./read.txt', 'utf-8');
 
 console.log(fileContent);
+
+fs.writeFileSync('./out-put.txt', fileContent);
+console.log('File has been written');
