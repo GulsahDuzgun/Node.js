3c37b97461712dddec73594e4e798afd7ab207a3
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-29 13:20:00 +0300

    solution with pipe()

--- 02-How-Node-Works/streams.js
+++ 02-How-Node-Works/streams.js
@@ -18,6 +18,8 @@ const server = require('http').createServer();
 //Soluiton 2
 // Every ready chunked data  is emitted the data event end it's written to res and send with end event. But response is sent every chunced data and after it send with response the variable value remove and assign again. But the readable stream is faster than response so there is a backpressure issue. To solve that we should use pipe function on readable stream.
 
+/*
+
 server.on('request', (req, res) => {
   const readable = fs.createReadStream('./test-file.txt');
   readable.on('data', (chunk) => {
@@ -33,6 +35,16 @@ server.on('request', (req, res) => {
     res.statusCode(500);
     res.end('File not found');
   });
+});
+*/
+
+// Soution 3 -> Pipe(): With pipe() we send the data as fast as receiving it
+// Pipe operator is avaible on all readable streams and it allows us to pipe the output of a readable stream right into the input of a writable stream, it will fix the problem of backpressure because it will automatically handle the speed basically of the data is comming in and the data is going out.
+
+server.on('request', (req, res) => {
+  const readable = fs.createReadStream('./test-file.txt');
+  readable.pipe(res);
+  //readableSource.pipe(writeableDestination)
 });
 
 server.listen(9500, '127.0.0.1', () => {
