27a4a1bea0195292a73cb6c7fdf3215abaa6c60a
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-30 12:23:03 +0300

    add Promise.then().catch()

--- 03-Asynchronous-JS/index.js
+++ 03-Asynchronous-JS/index.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+//then method only works for fullfilled promise, rejected promise will catch on catch block
+
 fs.readFile('./dog.txt', 'utf-8', (err, data) => {
   if (err) return console.log('Not Found');
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
-      console.log(res.body.message);
-
+    .then((res) => {
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log('Random dog image is saved to file');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
