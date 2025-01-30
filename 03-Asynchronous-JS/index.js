008fe1af191da0553bbc97c99662f400e8796a82
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2025-01-30 13:26:42 +0300

    Building Promises

--- 03-Asynchronous-JS/index.js
+++ 03-Asynchronous-JS/index.js
@@ -3,18 +3,28 @@ const superagent = require('superagent');
 
 //then method only works for fullfilled promise, rejected promise will catch on catch block
 
-fs.readFile('./dog.txt', 'utf-8', (err, data) => {
-  if (err) return console.log('Not Found');
+//Can be make a Promise Chain by returning Promise and take fullfilled value with then function
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image is saved to file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePromise = (filePath) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) reject(err?.message);
+      resolve(data);
     });
-});
+  });
+
+const writeToFilePromise = (filePath, data) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, (err) => {
+      if (err) reject('There is an error while writing the file');
+    });
+  });
+
+readFilePromise('./dog.txt')
+  .then((data) =>
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+  )
+  .then((res) => writeToFilePromise('dog-img.txt', res.body.message))
+  .catch((err) => {
+    console.log(err);
+  });
