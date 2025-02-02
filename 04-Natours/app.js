60e23b4491474562195090cfac3ac88bd1153f77
GulsahDuzgun <178773259+RoseMedford@users.noreply.github.com>
2025-02-02 12:19:16 +0300

    Handling Get Requests

--- 04-Natours/app.js
+++ 04-Natours/app.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
 const express = require('express');
+const { json } = require('stream/consumers');
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).json({ app: 'natours', auhor: 'Gulsah Duzgun' });
 });
 
-app.post('/', (req, res) => {
-  res.send('Server Post request');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(9500, () => {
