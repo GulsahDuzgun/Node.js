ffe24a4da37a64203712a844de5b548530b0655e
GulsahDuzgun <65841330+GulsahDuzgun@users.noreply.github.com>
2025-02-28 18:33:38 +0300

    add gte|gt|lt|lte filter
    Co-Authored-By: Rose Medford <178773259+RoseMedford@users.noreply.github.com>

--- 04-Natours/controller/tourController.js
+++ 04-Natours/controller/tourController.js
@@ -2,13 +2,17 @@ const Tour = require('../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    //MongoDB Query collection.find({property:val})
+    //MongoDB Query collection.find({property:val})if there is gte|gt|lt|lte {property: {operator:value}}
     //const tours = await Tour.find().where('difficulty').equals('easy');
     const queryObj = { ...req.query };
     const excludedFieds = ['page', 'sort', 'limit', 'fields'];
     excludedFieds.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     res.status(200).json({
