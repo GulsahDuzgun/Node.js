c49685f6e7dc720ad12b0977e6fd36725d4ad43c
GulsahDuzgun <65841330+GulsahDuzgun@users.noreply.github.com>
2025-03-04 12:33:39 +0300

    document Middleware

--- 04-Natours/models/tourModel.js
+++ 04-Natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -65,6 +67,24 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+//Document Middleware is saved before save() and create() functions
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  console.log(this);
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log('Post Middleware');
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
