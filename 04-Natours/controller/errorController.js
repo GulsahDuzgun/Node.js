2c1216cae57d3dc8e69da5cc5351258815246288
GulsahDuzgun <65841330+GulsahDuzgun@users.noreply.github.com>
2025-03-08 16:22:11 +0300

    Errors duirng Development and Production
    Co-Authored-By: Rose Medford <178773259+RoseMedford@users.noreply.github.com>

--- 04-Natours/controller/errorController.js
+++ 04-Natours/controller/errorController.js
@@ -1,10 +1,33 @@
 module.exports = (err, req, res, next) => {
-  console.log("If there is 4 paramater then it's a error handler middleware");
   err.status = err.status || 'fail';
   err.statusCode = err.statusCode || 500;
 
+  if (process.env.NODE_ENV === 'DEV') {
+    sendErrorDEV(res, err);
+  } else if (process.env.NODE_ENV === 'PROD') {
+    sendErrorProd(res, err);
+  }
+};
+
+const sendErrorDEV = (res, err) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
 };
