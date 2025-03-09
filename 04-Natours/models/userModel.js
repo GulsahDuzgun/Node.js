ba4718c1928718a303db01518582956f82e382a3
GulsahDuzgun <65841330+GulsahDuzgun@users.noreply.github.com>
2025-03-09 23:52:49 +0300

    Modelling Users
    Co-Authored-By: Rose Medford <178773259+RoseMedford@users.noreply.github.com>

---
+++ 04-Natours/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    requred: [true, 'User must has a name'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    requred: [true, 'User must has a email address'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    requred: [true, 'Password is required'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    requred: [true, 'Confirm Password is required'],
+  },
+  photo: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = Tour;
